a3ef196131b0f42a3e992d1d3e166f5c4df6913e
Sylvere Bastien <sylvere.bastien@gmail.com>
2016-07-13 15:51:05 +0200

    step function done

--- chipseq/Parser.ml
+++ chipseq/Parser.ml
@@ -1,3 +1,5 @@
+open Core.Std
+
 module Lines : sig
 
 type state
@@ -8,6 +10,23 @@ end
 
 = struct
 
+type state =
+	| Current_line of string
+	| Finished
 
+let initial_state = Current_line ""
+
+let step sthg st = match sthg, st with
+ | _, Finished -> (Finished, [])
+ | None, Current_line l -> (Finished, [l])
+ | Some sentence, Current_line l ->
+  match String.rsplit2 sentence ~on:'\n' with
+   | None -> (Current_line (l ^ sentence), [])
+   | Some (b, a) -> 
+     match String.split b ~on:'\n' with
+     | [] -> assert false (* [split] never returns an empty list *)
+     | h :: t -> (Current_line a, (l ^ h) :: t)
 
 end
+
+
